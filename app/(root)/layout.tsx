15fefe482365c846382b951eb64ff8fcf24e3cf3
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-23 07:13:35 +0500

    layout added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,11 @@
 import Auth from '@/components/auth'
+import { Toaster } from '@/components/ui/sonner'
 import { authOptions } from '@/lib/auth-options'
 import { getServerSession } from 'next-auth'
 import React from 'react'
+import NextTopLoader from 'nextjs-toploader'
+import Sidebar from '@/components/sidebar/sidebar'
+import FollowBar from '@/components/shared/follow-bar'
 
 interface Props {
     children: React.ReactNode
@@ -19,7 +23,31 @@ const Layout = async ({ children }: Props) => {
     }
 
     return (
-        <div>{children}</div>
+        <div className='lg:container h-screen mx-auto lg:max-w-7xl'> 
+            <div className='flex'>
+
+                <Sidebar/>
+                <div className='flex flex-1 border-x-[1px] border-neutral-800 lg:mx-4 ml-1'>
+                    <div className='w-full'>
+                        <NextTopLoader
+                            color='#2299DD'
+                            initialPosition={0.08}
+                            crawlSpeed={200}
+                            height={3}
+                            crawl={true}
+                            showSpinner={true}
+                            easing='ease'
+                            speed={200}
+                            shadow={'0 0 10px #2299DD, 0 0 5px #2299DD'}
+                        />
+                        {children}
+                        <Toaster/>
+                    </div>
+                </div>
+                <FollowBar/>
+
+            </div>
+        </div>
     )
 }
 
