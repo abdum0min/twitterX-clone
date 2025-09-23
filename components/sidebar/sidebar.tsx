5946abad5606cf52a77dff4067355fbfa7c9e298
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-23 07:25:09 +0500

    some chnages

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,12 +1,38 @@
 'use client'
 
+import { Bell, Home, User } from "lucide-react"
 import { useSession } from "next-auth/react"
 
 const Sidebar = () => {
-    const { data: session, status } = useSession()
+    const { data: session, status } : any  = useSession()
+
+    const sidebarItems = [
+        {
+            label: 'Home',
+            path: '/',
+            icon: Home
+        },
+        {
+            label: 'Notifications',
+            path: `/notlifications/${status === 'authenticated' && session?.currentuser?._id}`,
+            icon: Bell
+        },
+        {
+            label: 'Profile',
+            path: `/profile/${status === 'authenticated' && session?.currentuser?._id}`,
+            icon: User
+        }
+    ]
 
     return (
-        <div>Sidebar</div>
+        <section className="sticky top-0 left-0 h-screen lg:w-[266px], w-fit flex flex-col justify-between py-4">
+            <div className="flex flex-col space-y-2">
+                {/* Mobile sidebar */}
+                <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-sky-300 hover:bg-opacity-10 cursor-pointer transition">
+                    <Image/>
+                </div>
+            </div>
+        </section>
     )
 }
 
