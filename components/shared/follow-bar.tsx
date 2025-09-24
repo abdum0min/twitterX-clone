2af6b706ec746efa84d5d6c4a9c3137501d935c1
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-24 23:23:43 +0500

    some changes

--- components/shared/follow-bar.tsx
+++ components/shared/follow-bar.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Button from '../ui/button'
+import { Loader2 } from 'lucide-react'
 
 const FollowBar = () => {
+    let isLoaded = true
+
     return (
-        <div>FollowBar</div>
+        <div className="py-4 hidden lg:block w-[266px]">
+            <div className='bg-neutral-800 rounded-xl p-4'>
+                <div className='flex items-center justify-between'>
+                    <h2 className='text-white text-xl font-semibold'>Who to follow</h2>
+                    <Button  label='See all' secondary classNames='h-[30px] p-0 w-fit px-3 text-sm'/>
+                </div>  
+
+                {isLoaded ? (
+                    <div className='flex justify-center items-center h-24'>
+                        <Loader2 className='animate-spin text-sky-500'/>
+                    </div>
+                ): (
+                    ''
+                )}
+            </div>
+        </div>
     )
 }
 
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,8 +10,9 @@ interface ButtonProps {
     outline?: boolean
     type?: "button" | "submit" 
     onClick?: () => void
+    classNames?: string
 }
-const Button = ({ label, secondary, fullWidth, large, disabled, outline, type, onClick } : ButtonProps) => {
+const Button = ({ label, secondary, fullWidth, large, disabled, outline, type, onClick, classNames } : ButtonProps) => {
     return (
         <button
             disabled={disabled}
@@ -21,7 +22,8 @@ const Button = ({ label, secondary, fullWidth, large, disabled, outline, type, o
                 fullWidth ? "w-full" : "w-fit",
                 secondary ? "bg-white text-black" : "bg-sky-500 text-white",
                 large ? "text-xl px-5 py-3" : "text-md px-4 py-3",
-                outline ? "bg-transparent border-slate-600 text-sky-500 hover:bg-slate-800/40" : ""
+                outline ? "bg-transparent border-slate-600 text-sky-500 hover:bg-slate-800/40" : "",
+                classNames
             )}
         >
             {label}
