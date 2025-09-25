cdd8e77b97ddf61df92acb66a0384b1c63637aff
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-25 06:57:57 +0500

    user added

--- components/shared/user.tsx
+++ components/shared/user.tsx
@@ -3,12 +3,17 @@ import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
 const User = () => {
     return (
-        <div className='flex gap-3 items-center justify-between'>
-            <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-3 items-center justify-between cursor-pointer hover:bg-neutral-200/10 transition py-2 px-3 rounded-md'>
+            <div className='flex gap-2 cursor-pointer items-center'>
                 <Avatar>
                     <AvatarImage/>
                     <AvatarFallback>CB</AvatarFallback>
                 </Avatar>
+
+                <div className='flex flex-col'>
+                    <p className='text-white font-semibold text-sm line-clamp-1'>Samar</p>
+                    <p className='text-neutral-400 text-sm line-clamp-1'>@samar0811</p>
+                </div>
             </div>
         </div>
     )
