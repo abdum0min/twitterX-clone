45040d7e6d4b5fbc8450378f6f40608a41b9dbed
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-30 09:24:24 +0500

    new changes in home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,16 @@
+import Form from '@/components/shared/form'
 import Header from '@/components/shared/header'
+import { authOptions } from '@/lib/auth-options'
+import { getServerSession } from 'next-auth'
 import React from 'react'
 
-const HomePage = () => {
+const HomePage = async () => {
+    const session: any = await getServerSession(authOptions)
+
     return (
         <>
-            <Header label='Home'/>
+            <Header label='Home' />
+            <Form user={JSON.parse(JSON.stringify(session.currentuser))} placeholder="What's on your mind" />
         </>
     )
 }
--- components/shared/form.tsx
+++ components/shared/form.tsx
@@ -1,4 +1,9 @@
+"use client"
+
 import { IUser } from '@/types'
+import { useState } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import Button from '../ui/button'
 // import React, { Dispatch, SetStateAction } from 'react'
 
 
@@ -12,8 +17,45 @@ interface Props {
 }
 
 const Form = ({ user, placeholder }: Props) => {
+    const [body, setBody] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const onSubmit = async () => {
+
+    }
+
     return (
-        <div>form</div>
+        <div className='border-b-[1px] border-neutral-800 px-5 py-2'>
+            <div className='flex flex-row gap-4'>
+                <Avatar>
+                    <AvatarImage src={user?.profileImage}/>
+                    <AvatarFallback>{user?.name[0]}</AvatarFallback>
+                </Avatar>
+
+                <div className='w-full'>
+                    <textarea
+                        className='disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white h-[50px]'
+                        placeholder={placeholder}
+                        disabled={isLoading}
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                        onKeyDown={(e) => e.key === "Enter" && onSubmit()}
+                    ></textarea>
+                    <hr className='opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition' />
+
+
+                    <div className='mt-4 flex flex-row justify-end'>
+                        <Button
+                            label={'Post'}
+                            classNames='px-8'
+                            disabled={isLoading || !body}
+                            onClick={onSubmit}
+                        />
+                    </div>
+                </div> 
+            </div>
+        </div>
     )
 }
 
